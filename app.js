88c8bcc675411a6f86e2c898941aeb822e6bb20e
Telman Mammadov <tehsilelm20212@gmail.com>
2023-09-23 06:15:13 +0000

    remove semicolon

--- app.js
+++ app.js
@@ -6,27 +6,27 @@ $(document).ready(function () {
             async function myweather() {
                 let responce = await fetch(url)
                 let data = await responce.json()
-                $("#city").html(data.name);
-                $("#temp").html(data.main.temp);
-                $("#feels").html(data.main.feels_like);
-                $("#myweather").val("");
-                console.log(data);
+                $("#city").html(data.name)
+                $("#temp").html(data.main.temp)
+                $("#feels").html(data.main.feels_like)
+                $("#myweather").val("")
+                console.log(data)
             }
             myweather()
         }
     });
 
     $("#btn").click(function (e) {
-        e.preventDefault();
+        e.preventDefault()
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${$('#myWeather').val()}&appid=fed35d00aceba09ad5ed047e14a4d286&units=metric`
         async function myweather() {
             let responce = await fetch(url)
             let data = await responce.json()
-            $("#city").html(data.name);
-            $("#temp").html(data.main.temp);
-            $("#feels").html(data.main.feels_like);
-            $("#myWeather").val("");
-            console.log(data);
+            $("#city").html(data.name)
+            $("#temp").html(data.main.temp)
+            $("#feels").html(data.main.feels_like)
+            $("#myWeather").val("")
+            console.log(data)
         }
         myweather()
     });
